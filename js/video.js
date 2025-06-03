0e6269a61ed256b5a9ecb4c28e432460afc025d1
ArnavAsif <ifas092@gmail.com>
2025-06-03 22:48:23 +0600

    second

--- js/video.js
+++ js/video.js
@@ -1,13 +1,13 @@
 const loadCategories = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
         .then(res => res.json())
-        .then(data => displayCatagories(data.categories))
+        .then(data => displayCategories(data.categories))
         .catch((error) => console.log(error))
 };
-const displayCatagories = (categories) => {
-    
+const displayCategories = (categories) => {
+
     const buttonContainer = document.getElementById('categories')
-    
+
     categories.forEach(item => {
         // making button api
         const button = document.createElement('button');
@@ -19,4 +19,18 @@ const displayCatagories = (categories) => {
     })
 }
 
+const videoLoader = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then(res => res.json())
+        .then(data => displayVideo(data.videos))
+        .catch((error) => console.log(error))
+};
+
+const displayVideo = (videos) => {
+    videos.forEach(video => {
+
+    })
+};
+
+videoLoader();
 loadCategories();
